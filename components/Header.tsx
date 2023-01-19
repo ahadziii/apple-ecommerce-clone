29728fffc4ebec1ae0f1a9c28464f50a8d15b1ad
Edem Ahadzi <ahadzi.airdem@gmail.com>
2023-01-19 20:39:22 +0000

    feat: retrieve from store using the selectors

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,14 @@ import Image from "next/image"
 import Link from "next/link"
 import React from "react"
 import { SearchIcon, ShoppingBagIcon, UserIcon } from "@heroicons/react/outline"
+import { selectBasketItems } from "../redux/basketSlice"
+import { useSelector } from "react-redux"
 
 function Header() {
   const session = false
+
+  const items = useSelector(selectBasketItems)
+
   return (
     <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4 ">
       <div className="flex items-center justify-center md:w-1/5">
@@ -30,7 +35,7 @@ function Header() {
         <Link href="/checkout">
           <div className="relative">
             <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
-              5
+              {items.length}
             </span>
             <ShoppingBagIcon className="headerIcon" />
           </div>
