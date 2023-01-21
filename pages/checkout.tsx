f4e006238e192dca4921d0889b0c8d7ea5d931d2
Edem Ahadzi <ahadzi.airdem@gmail.com>
2023-01-21 23:40:10 +0000

    feat: build checkout product component

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router"
 import React, { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import Button from "../components/Button"
+import CheckoutProduct from "../components/CheckoutProduct"
 import Header from "../components/Header"
 import { selectBasketItems } from "../redux/basketSlice"
 
@@ -15,7 +16,10 @@ function Checkout() {
 
   useEffect(() => {
     const groupedItems = items.reduce((results, item) => {
-      ;(results[item._id] = results[item._id] || []).push(item)
+      // if(!results[item._id]){
+      //     results[item._id]=[]
+      // }
+      (results[item._id] = results[item._id] || []).push(item)
       return results
     }, {} as { [key: string]: Product[] })
 
@@ -23,16 +27,16 @@ function Checkout() {
   }, [items])
 
   return (
-    <div>
+    <div className="min-h-screen overflow-hidden bg-[#E7ECEE]">
       <Head>
         <title>Bag - Apple</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
-        <div>
+      <main className="mx-auto max-w-5xl pb-24">
+        <div className="px-5">
           <h1 className="my-4 text-3xl font-semibold lg:text-4xl">
-            {items.length > 0 ? "Review you bag." : " Your bag is empty."}
+            {items.length > 0 ? "Review your bag." : " Your bag is empty."}
           </h1>
           <p className="my-4">Free delivery and free returns.</p>
 
@@ -44,7 +48,12 @@ function Checkout() {
           )}
         </div>
 
-        {items.length > 0 && <div></div>}
+        {items.length > 0 &&
+         <div className="mx-5 md:mx-8">
+            {Object.entries(groupedItemsInBasket).map(([key, items])=>(
+                <CheckoutProduct key={key} items={items} id={key}/>
+            ))}
+        </div>}
       </main>
     </div>
   )
